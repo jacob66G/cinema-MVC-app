38b19ac273c2139e009e217c4f988404336a2e01
JAKUB GERWEL <kuba.gerwel@gmail.com>
2025-03-06 12:00:44 +0100

    testy jednostkowe movieValidationService

--- Cinema/src/test/java/com/example/Cinema/service/Validators/MovieValidationServiceTest.java
+++ Cinema/src/test/java/com/example/Cinema/service/Validators/MovieValidationServiceTest.java
@@ -1,37 +1,42 @@
 package com.example.Cinema.service.Validators;
 
 import com.example.Cinema.model.Dto.MovieDto;
+import com.example.Cinema.model.Ticket;
 import com.example.Cinema.repository.MovieRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.example.Cinema.repository.TicketRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class MovieValidationServiceTest {
 
     @Mock
     MovieRepository movieRepository;
 
+    @Mock
+    TicketRepository ticketRepository;
+
     @InjectMocks
     MovieValidationService underTest;
 
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
-    void shouldTitleExist() {
+    void isTitleValid_titleValid_returnsTrue() {
         //given
         MovieDto movieDto = new MovieDto();
-        movieDto.setTitle("TEST");
+        movieDto.setTitle("title");
         movieDto.setIdmovie(1L);
 
-        when(movieRepository.existsByTitle("TEST", 1L)).thenReturn(false);
+        when(movieRepository.existsByTitle("title", 1L)).thenReturn(false);
 
         //when
         boolean result = underTest.isTitleValid(movieDto);
@@ -41,7 +46,7 @@ class MovieValidationServiceTest {
     }
 
     @Test
-    void shouldTitleNotValid() {
+    void isTitleValid_titleNoValid_returnsFalse() {
         //given
         MovieDto movieDto = new MovieDto();
         movieDto.setTitle("TEST");
@@ -54,5 +59,61 @@ class MovieValidationServiceTest {
 
         //then
         assertFalse(result);
+    }
+
+    @Test
+    void isTitleValid_whenIdIsNull_returnsFalseIfTitleExists() {
+        //given
+        MovieDto movieDto = new MovieDto();
+        movieDto.setTitle("TEST");
+        movieDto.setIdmovie(null);
+
+        when(movieRepository.existsByTitle("TEST", null)).thenReturn(true);
+
+        //when
+        boolean result = underTest.isTitleValid(movieDto);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void isMovieCanBeEdit_whenTicketsExist_returnsFalse() {
+        //given
+        long movieId = 1L;
+
+        when(ticketRepository.findAllByProgramme_Movie_Idmovie(movieId)).thenReturn(List.of(new Ticket(), new Ticket()));
+
+        //when
+        boolean result = underTest.isMovieCanBeEdit(movieId);
+
+        //then
+        assertFalse(result);
+    }
+
+    @Test
+    void isMovieCanBeEdit_whenNoTickets_returnsTrue() {
+        //given
+        long movieId = 1L;
+
+        when(ticketRepository.findAllByProgramme_Movie_Idmovie(movieId)).thenReturn(new ArrayList<>());
+
+        //when
+        boolean result = underTest.isMovieCanBeEdit(movieId);
+
+        //then
+        assertTrue(result);
+    }
+
+    @Test
+    void isMovieCanBeEdit_whenIdIsNull_returnsTrue() {
+        //given
+        Long movieId = null;
+
+        //when
+        boolean result = underTest.isMovieCanBeEdit(movieId);
+
+        //then
+        assertTrue(result);
     }
 }
